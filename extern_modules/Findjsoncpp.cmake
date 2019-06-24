cf6282a3c6197c6da65678cf0a9de1f0e87edc3f
Guanyu Li <guanyu.li@outlook.com>
2019-06-24 18:36:31 +0800

    modify for mac os

--- extern_modules/Findjsoncpp.cmake
+++ extern_modules/Findjsoncpp.cmake
@@ -3,5 +3,5 @@ SET(jsoncpp_FOUND 1)
 
 #这里可以用FIND_LIBRARY FIND_PATH命令根据备选路径，版本号来真正查找，但是没有必要这么灵活
 SET(jsoncpp_INCLUDE_DIR /usr/local/include)
-SET(jsoncpp_LIBRARY /usr/local/lib64)
+SET(jsoncpp_LIBRARY /usr/local/lib)
 
